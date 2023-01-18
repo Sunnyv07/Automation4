6541b414bf366978e5335182c1930895bc52ecbd
Shubhank07 <shubhank31093@gmail.com>
2023-01-18 12:59:19 +0530

    Screenshot functionality added

--- src/main/java/features/demo.feature
+++ src/main/java/features/demo.feature
@@ -3,3 +3,8 @@ Feature: Test
   Scenario: test 1
     Given Open Web Page
     Then Print Message
+
+  Scenario: test 2
+    Given Open Web Page
+    Then Edit text fields
+    Then Print Message
--- src/main/java/steps/MyStepdefs.java
+++ src/main/java/steps/MyStepdefs.java
@@ -1,24 +1,44 @@
 package steps;
 
+import com.beust.jcommander.Parameterized;
+import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MyStepdefs {
-    @io.cucumber.java.en.Given("^Open Web Page$")
-    public void openWebPage() {
-        System.out.println("Open");
+    private WebDriver driver;
+
+    public MyStepdefs()
+    {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
+        driver=new ChromeDriver();
         driver.manage().window().maximize();
+    }
+    
+    @io.cucumber.java.en.Given("^Open Web Page$")
+    public void openWebPage() throws IOException {
+        System.out.println("Open");
         driver.get("http://omayo.blogspot.com/");
-        driver.findElement(By.name("q")).sendKeys("Sunny");
-        driver.close();
     }
 
     @io.cucumber.java.en.Then("^Print Message$")
     public void printMessage() {
         System.out.println("Code Exited");
+    }
+
+    @Then("Edit text fields")
+    public void editTextFields() throws IOException {
+        driver.findElement(By.name("q")).sendKeys("Sunny");
+        driver.findElement(By.id("ta1")).sendKeys(("Vishwakarma"));
+        TakesScreenshot scrShot =((TakesScreenshot)driver);
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile=new File("C:\\Users\\Admin\\OneDrive\\Pictures\\Screenshots\\Automation Screenshots\\New Bitmap Image.jpg");
+        FileUtils.copyFile(SrcFile, DestFile);
+        driver.close();
     }
 }
