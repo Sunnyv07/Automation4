bfaba9b6af26e0c9eeac3720190b86f3446cf6eb
Shubhank07 <shubhank31093@gmail.com>
2023-01-18 16:30:09 +0530

    Refinement

--- src/main/java/features/demo.feature
+++ src/main/java/features/demo.feature
@@ -3,14 +3,17 @@ Feature: Test
   Scenario: test 1
     Given Open Web Page
     Then Print Message
+    Then Close Browser
 
   Scenario: test 2
     Given Open Web Page
     Then Edit text fields
     Then Print Message
+    Then Close Browser
 
   Scenario: test 3
     Given Open Web Page
     Then Edit text fields
     Then Click Login
     Then Print Message
+    Then Close Browser
--- src/main/java/steps/MyStepdefs.java
+++ src/main/java/steps/MyStepdefs.java
@@ -1,18 +1,14 @@
 package steps;
 
-import com.sun.jdi.connect.spi.TransportService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Capabilities;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class MyStepdefs {
     private final WebDriver driver;
@@ -25,7 +21,7 @@ public class MyStepdefs {
     }
     
     @Given("^Open Web Page$")
-    public void openWebPage() throws IOException {
+    public void openWebPage() {
         System.out.println("Open");
         driver.get("https://app.sekel.tech/login");
     }
@@ -33,19 +29,25 @@ public class MyStepdefs {
     @io.cucumber.java.en.Then("^Print Message$")
     public void printMessage() throws IOException {
         screenShotFunction();
-        System.out.println("Code Exited");
+        System.out.println("Test Completed");
     }
 
     @Then("Edit text fields")
-    public void editTextFields() throws IOException {
+    public void editTextFields() {
         driver.findElement(By.xpath("//input[@placeholder='Please enter email']")).sendKeys("Sunny");
         driver.findElement(By.xpath("//input[@placeholder='Please enter password']")).sendKeys("Vishwakarma");
     }
 
     public void screenShotFunction() throws IOException {
+        int screenCount=0;
         TakesScreenshot scrShot =((TakesScreenshot)driver);
         File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
-        File DestFile=new File("C:\\Users\\Admin\\OneDrive\\Pictures\\Screenshots\\Automation Screenshots\\New Bitmap Image1.jpg");
+        File DestFile=new File("C:\\Users\\Admin\\OneDrive\\Pictures\\Screenshots\\Automation Screenshots\\"+screenCount+".jpg");
+        while (DestFile.exists())
+        {
+            screenCount=screenCount+1;
+            DestFile=new File("C:\\Users\\Admin\\OneDrive\\Pictures\\Screenshots\\Automation Screenshots\\"+screenCount+".jpg");
+        }
         FileUtils.copyFile(SrcFile, DestFile);
     }
 
@@ -53,5 +55,10 @@ public class MyStepdefs {
     public void clickLogin() {
         WebElement clickable =driver.findElement(By.xpath("//div[@class='submit flex-vertical-align']//input[1]"));
         clickable.click();
+    }
+
+    @Then("Close Browser")
+    public void closeBrowser() {
+        driver.close();
     }
 }
